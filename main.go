127c24d0d88d5e8856a2e1812cfeaad61892abea
NekruzRakhimov <69744116+NekruzRakhimov@users.noreply.github.com>
2021-10-07 23:51:35 +0500

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+func main() {
+	RunAllRoutes()
+}
+
+
+func RunAllRoutes() {
+	r := gin.Default()
+
+	// Исползование CORS
+	//r.Use(controller.CORSMiddleware())
+
+	// Запуск роутов
+	runAllRoutes(r)
+
+	// Запуск сервера
+	_ = r.Run("localhost: " + os.Getenv("PORT"))
+}
+
+func runAllRoutes(r *gin.Engine) {
+	//r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/ping", PingPong)
+}
+
+// PingPong Проверка
+func PingPong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"ping": "pong"})
+}
